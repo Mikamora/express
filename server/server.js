e5c57909586024ee940677df82cc4cba39e0ce6b
Michael Richardson <mor7991@yahoo.com>
2021-03-08 11:05:45 -0600

    advanced

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 let express = require("express");
 let path = require("path");
+let fs = require("fs");
+let bodyParser = require("body-parser");
 let app = express(); 
+
+
+//step1
+
+// app.get("/", (req, res)=>{
+//     res.send("Hello from the web server side"); 
+// })
+
+//step3
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/formsubmissions", (req, res, next)=> {
+    console.log(req.body.email);
+    console.log(req.body.name); 
+    fs.appendFileSync("form.json", `{ "Email": "${req.body.email}",\n "Name": "${req.body.name}" }`); 
+    res.send(`Name: ${req.body.name}\n Email: ${req.body.email}`)
+    next();
+})
+
+//step2
 
 app.use((req, res, next) => {
     console.log(req.url);
     next();
 })
-app.get("/", (req, res)=>{
-    res.send("Hello from the web server side"); 
-})
+
 
 app.use(express.static(path.join(__dirname, "../public")));
 
